7d573c13497ac702d4809e39b4800efdbadcba5e
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2013-06-28 05:56:15 +0900

    JSONP用のテスト追加

---
+++ test/jsonp/data001.js
@@ -0,0 +1,9 @@
+zip2addr([
+  { zip: '3000000', pref: '福岡県', city: 'テスト市テスト町',         town: 'テスト'       },
+  { zip: '3000001', pref: '福岡県', city: 'サンプル市サンプル町',     town: 'サンプル'     },
+  { zip: '3000002', pref: '福岡県', city: 'ajaxzip市ajaxzip町',       town: '住所自動補完' },
+  { zip: '3000003', pref: '熊本県', city: 'ajaxzip2市ajaxzip2町',     town: '住所自動補完' },
+  { zip: '3000004', pref: '大分県', city: 'ユニットテスト市',         town: 'QUnit'        },
+  { zip: '3000005', pref: '佐賀県', city: '自動テスト市自動テスト町', town: 'QUnit1.10'    },
+  { zip: '3000006', pref: '長崎県', city: 'ほげ市ホゲ町',             town: 'ほげふが'     }
+]);
---
+++ test/jsonp_test.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  module('JSONP形式での自動補完');
+
+  QUnit.moduleStart(function(name) {
+    if (name.name == 'JSONP形式での自動補完') {
+      $.fn.zip2addr.defaultOptions.type   = 'jsonp';
+      $.fn.zip2addr.defaultOptions.path   = 'jsonp/data001.js';
+      $.fn.zip2addr.defaultOptions.callback = function(data) {
+        var result = {};
+        for (var key in data) {
+          var z = data[key];
+          result[z.zip] = [ z.pref, z.city, z.town, '' ];
+        }
+
+        return result;
+      };
+    }
+    else {
+      $.fn.zip2addr.defaultOptions.type   = 'json';
+      $.fn.zip2addr.defaultOptions.path   = '/ajaxzip2/data/zip-%ZIP%.json';
+      $.fn.zip2addr.defaultOptions.callback = null;
+    }
+  });
+
+  asyncTest('JSONP形式で住所データを取得する', function() {
+    expect(4);
+
+    $('#zip').val('3000000').zip2addr();
+    setTimeout(function() {
+      equal($('#pref').  val(), '福岡県',           '都道府県が正しくセットされている');
+      equal($('#city').  val(), 'テスト市テスト町', '市区町村が正しくセットされている');
+      equal($('#area').  val(), 'テスト',           '町域が正しくセットされている');
+      equal($('#street').val(), '',                 '番地が正しくセットされている');
+      start();
+    }, 100);
+  });
+});
